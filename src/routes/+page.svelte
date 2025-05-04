58deec7797349c5bdbb781a5251085ee3f90f5d7
joudprogs <joudalshehri25@gmail.com>
2025-05-04 22:21:19 +0300

    sections creation

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Content from "./components/content.svelte";
+import Header from "./components/header.svelte";
+  import Herosec from "./components/herosec.svelte";
+
+</script>
+<Header></Header>
+<Herosec></Herosec>
+<Content></Content>
